4b3f9feff3464a5ca4251eff18edd06e110b657a
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-26 15:06:01 +0530

    Code Updated

--- Lane Lines/Lane_Lines.py
+++ Lane Lines/Lane_Lines.py
@@ -1,1 +1,71 @@
+# This File Will detect the Lane Lines in The Video Frame unlike of the image.
+import cv2
+import numpy as np
+
+def make_points(image, line):
+    slope, intercept = line
+    y1 = int(image.shape[0])
+    y2 = int(y1*3/5)        
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return [[x1, y1, x2, y2]]
+
+def average_slope_intercept(image, lines):
+    left_fit    = []
+    right_fit   = []
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            fit = np.polyfit((x1,x2), (y1,y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0: 
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+        
+    left_fit_average  = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line  = make_points(image, left_fit_average)
+    right_line = make_points(image, right_fit_average)
+    averaged_lines = [left_line, right_line]
+    return averaged_lines
+
+def canny(image):
+    gray=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    blur=cv2.GaussianBlur(gray,(5,5),0)
+    canny=cv2.Canny(blur,50,150)
+    return canny
+
+def display_lines(image,lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+    return line_image
+
+def region_of_interest(image):
+    height = canny.shape[0]
+    mask = np.zeros_like(canny)
+
+    triangle = np.array([[
+    (200, height),
+    (550, 250),
+    (1100, height),]], np.int32)
+
+    cv2.fillPoly(mask, triangle, 255)
+    masked_image = cv2.bitwise_and(image, mask) 
+    return masked_image
+
+image = cv2.imread('test_image.mp4')
+lane_image= np.copy(image)
+canny=canny(lane_image)
+cropped_image=region_of_interest(canny)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image=display_lines(lane_image, averaged_lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 0)
+cv2.imshow('result',combo_image)
+cv2.waitKey(0)
 
